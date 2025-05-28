9f233afafef2f7382eb4f9adda5315c7af03329f
Eileen <oceanapple580@gmail.com>
2025-05-28 16:44:37 -0400

    Tried separating into different react components..?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import data from './members_details.json'
+import Footer from './footer'
+import AllMembers from './displayMember'
 
 function App() {
   return (
@@ -12,58 +14,8 @@ function App() {
       <div className='learn-about'>
         <h1 className='title'>Learn About the Orientation Committee!</h1>
       </div>
-
-      <div className="info">
-        <div className="section">
-          <h1 className='position'>Co-Orientation Chairs</h1>
-          <div className='members'>
-            {data.filter(member => member.position === "Co-Orientation Chair").map((member) => (
-              <div key={member.name} className='memberCard'>
-                <div className = 'memberCard-inner'>
-                  <div className='memberCard-front'>
-                    <h2 className='memberName'>{member.name}</h2>
-                    <img src={member.picture} alt={member.name} className='memberPhoto'>
-                    </img>
-                  </div>
-                  <div className='memberCard-back'>
-                    <h2>{member.position}</h2>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="section">
-          <h1 className='position'>Vice Chairs</h1>
-          <div className='members'>
-            {data.filter(member => member.position.includes("VC")).map((member) => (
-              <div key={member.name} className='memberCard'>
-                <div className = 'memberCard-inner'>
-                  <div className='memberCard-front'>
-                    <h2 className='memberName'>{member.name}</h2>
-                    <img src={member.picture} alt={member.name} className='memberPhoto'>
-                    </img>
-                  </div>
-                  <div className='memberCard-back'>
-                    <h2>{member.position}</h2>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      <footer>
-        <h3 className='footer-title'>Find us here!</h3>
-        <ul>
-          <li><a href="https://discord.gg/hJT44RcK" target = "_blank">Discord</a></li>
-          <li><a href = "https://www.instagram.com/froshweek/" target = "_blank">Instagram</a></li>
-          <li><a href = "https://linktr.ee/froshweek" target = "_blank">Linktree</a></li>
-        </ul>
-      </footer>
-
+      <AllMembers />
+      <Footer />
     </div>
   );
 }
